b8e42b63a06e2f6fec3864ff45a90a255858477a
itsmesunky <itisp_06@naver.com>
2024-12-07 12:27:12 +0900

    feat: 유효성 검증 포함 Input 컴포넌트 생성

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,26 +1,63 @@
-import { forwardRef } from 'react';
+import { useInput } from '@/hooks/useInput';
+import Button from '../Button';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   id?: string;
+  change?: () => boolean;
+  blur?: () => boolean;
   isError?: boolean;
   error?: string;
+  descrption?: string;
+  buttonText?: string;
+  buttonClick?: () => void;
+  validate?: (value: string) => boolean;
 }
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ label, id, isError, error, ...props }, ref) => {
-    return (
-      <div>
+export default function Input({
+  label,
+  id,
+  change,
+  blur,
+  isError,
+  error,
+  descrption,
+  buttonText,
+  buttonClick,
+  validate,
+  ...props
+}: InputProps) {
+  const { value, handleInputChange, handleInputBlur, hasError } = useInput(
+    '',
+    validate ? (value) => validate(value) : () => true,
+  );
+
+  return (
+    <>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
         <label htmlFor={id}>{label}</label>
-        <input ref={ref} id={id} {...props} />
-        {isError && (
-          <div>
-            <p>{error}</p>
-          </div>
+        <input
+          id={id}
+          value={value}
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          style={{ outline: 'none' }}
+          {...props}
+        />
+        {buttonText && (
+          <Button
+            type="button"
+            text={buttonText}
+            disable={hasError}
+            onClick={buttonClick}
+          />
         )}
       </div>
-    );
-  },
-);
-
-export default Input;
+      {descrption && (
+        <div>
+          <p style={{ fontSize: 12 }}>{descrption}</p>
+        </div>
+      )}
+    </>
+  );
+}
