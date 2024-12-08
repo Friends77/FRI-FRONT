909b7965a8e36a38e813e0afaed0cc2d0d92cdaf
jiwon <iamkjw77@gmail.com>
2024-12-09 00:38:02 +0900

    chore: 취약점 해결, axios, tsconfig 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import "./App.css";
+import { useEffect, useState } from 'react';
+import './App.css';
+import Axios from './apis/@core';
 
 function App() {
-  return <></>;
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    const fetchTest = async () => {
+      const { data } = await Axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1'
+      );
+
+      setState(data);
+    };
+
+    fetchTest();
+  }, []);
+
+  console.log(state);
+
+  return <>안녕</>;
 }
 
 export default App;
