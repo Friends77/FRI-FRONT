2ebc7fcdf5d3f53e60f522ab542ced50eeff2d5c
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-12-10 20:09:26 +0900

    feat: 로그인 유효성 검사 추가
    - 이메일: 이메일 패턴과 필수 항목 검사
    - 비밀번호: 필수 항목 검사
    
    - 이메일 정규표현식을 파일로 분리하기엔 하나뿐이라 우선 상수로 선언

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,8 +5,14 @@ type LoginType = {
   password: string;
 };
 
+const EMAIL_PATTERN = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 export default function LoginPage() {
-  const { register, handleSubmit } = useForm<LoginType>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginType>();
 
   const onSubmit: SubmitHandler<LoginType> = (data) => {
     console.log(data);
@@ -14,9 +20,26 @@ export default function LoginPage() {
   return (
     <main>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="email" {...register("email")} />
-        <input type="password" {...register("password")} />
-        <input type="submit" />
+        <input
+          type="email"
+          placeholder="이메일"
+          {...register("email", {
+            required: "이메일을 입력해주세요.",
+            pattern: {
+              value: EMAIL_PATTERN,
+              message: "이메일 형식이 아닙니다.",
+            },
+          })}
+        />
+        {errors.email && <span>{errors.email.message}</span>}
+        <input
+          type="password"
+          placeholder="비밀번호"
+          {...register("password", {
+            required: "비밀번호를 입력해주세요.",
+          })}
+        />
+        <button type="submit">제출</button>
       </form>
     </main>
   );
