23ff7ce0d65ab1e2ec1fb13cf60d3dcb18ea93e6
itsmesunky <itisp_06@naver.com>
2024-12-10 16:08:29 +0900

    route: 라우터 구축

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "../App";
+import { ROOT_PATH } from "../constants/routes";
+
+export default function AppRouter() {
+  const router = createBrowserRouter([
+    {
+      path: ROOT_PATH.ROOT,
+      element: <App />,
+      children: [],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
