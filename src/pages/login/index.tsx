159039cc4c0d9cdb3eca6225d88fe558aff9d635
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-12-10 18:45:21 +0900

    feat: 로그인 폼 생성

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,23 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type LoginType = {
+  email: string;
+  password: string;
+};
+
 export default function LoginPage() {
-  return <div>login</div>;
+  const { register, handleSubmit } = useForm<LoginType>();
+
+  const onSubmit: SubmitHandler<LoginType> = (data) => {
+    console.log(data);
+  };
+  return (
+    <main>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="email" {...register("email")} />
+        <input type="password" {...register("password")} />
+        <input type="submit" />
+      </form>
+    </main>
+  );
 }
