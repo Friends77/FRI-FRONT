3e416135e2e7ac1f0e9cca65d5795ca9f1fece1f
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-12-12 18:32:31 +0900

    style: 로그인 상태 확인용 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { Outlet } from "react-router";
 import "./App.css";
-import { useRecoilValue } from "recoil";
-import isLoggedInAtom from "./recoil/isLoggedIn/atom";
 
 function App() {
-  const isLoggedIn = useRecoilValue(isLoggedInAtom);
-
   return (
     <>
-      {/* 로그인 확인용으로 간단하게 만들어놨습니다! 머지하기 전에 이 코드는 삭제하고 진행하겠습니다 */}
-      <span>{isLoggedIn ? '로그인 O 👍' : '로그인 X 👎'}</span>
       <Outlet />
     </>
   );
