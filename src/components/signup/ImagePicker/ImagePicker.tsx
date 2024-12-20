e11294e2d17c0ff30a926d70fd0b8f3a2711a661
itsmesunky <itisp_06@naver.com>
2024-12-20 19:35:14 +0900

    feat: ImagePicker 컴포넌트 생성

---
+++ src/components/signup/ImagePicker/ImagePicker.tsx
@@ -0,0 +1,65 @@
+import { useRef, useState } from "react";
+import { useFormContext } from "react-hook-form";
+
+export interface IImagePickerProps {
+  name: string;
+}
+
+const ImagePicker = ({ name }: IImagePickerProps) => {
+  const { register, setValue } = useFormContext();
+
+  const [pickedImage, setPickedImage] = useState<string | null>(null);
+
+  const imageInput = useRef<HTMLInputElement>(null);
+
+  const buttonClickHandler = () => {
+    imageInput.current?.click();
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      setPickedImage(null);
+      setValue(name, null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      if (typeof fileReader.result === "string") {
+        setPickedImage(fileReader.result);
+      }
+    };
+
+    fileReader.readAsDataURL(file);
+
+    setValue(name, file);
+  };
+
+  return (
+    <div>
+      <div>
+        {pickedImage ? (
+          <img src={pickedImage} alt={`${name} preview`} />
+        ) : (
+          <p>기본 이미지</p>
+        )}
+      </div>
+      <input
+        id={name}
+        type="file"
+        {...register(name)}
+        ref={imageInput}
+        style={{ display: "none" }}
+        onChange={handleImageChange}
+      />
+      <button type="button" onClick={buttonClickHandler}>
+        이미지 변경
+      </button>
+    </div>
+  );
+};
+
+export default ImagePicker;
