4633642b6f1ea768bed154a31d281468c8fe5fda
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-12-21 16:17:02 +0900

    refactor: 로그인 페이지에 <InputField> 적용

--- src/components/auth/Form/LoginForm.tsx
+++ src/components/auth/Form/LoginForm.tsx
@@ -2,15 +2,17 @@ import { AUTH_ERROR_MSG } from "@/constants/message";
 import { AUTH_PATTERN } from "@/constants/pattern";
 import { useLogin } from "@/hooks/auth/useLogin";
 import { LoginDataType } from "@/types/auth";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+import InputField from "../Input/InputField";
 
 const LoginForm = () => {
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors },
-  } = useForm<LoginDataType>();
+  const methods = useForm<LoginDataType>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const { handleSubmit, setError } = methods;
 
   const { mutate } = useLogin({
     loginErrorHandler: () => {
@@ -25,29 +27,35 @@ const LoginForm = () => {
     mutate(data);
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="email"
-        placeholder="이메일"
-        {...register("email", {
-          required: AUTH_ERROR_MSG.EMAIL_REQUIRED,
-          pattern: {
-            value: AUTH_PATTERN.EMAIL,
-            message: AUTH_ERROR_MSG.EMAIL_PATTERN,
-          },
-        })}
-      />
-      {errors.email && <span>{errors.email.message}</span>}
-      <input
-        type="password"
-        placeholder="비밀번호"
-        {...register("password", {
-          required: AUTH_ERROR_MSG.PASSWORD_REQUIRED,
-        })}
-      />
-      {errors.password && <span>{errors.password.message}</span>}
-      <button type="submit">제출</button>
-    </form>
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <InputField
+          type="email"
+          label="이메일"
+          id="email"
+          name="email"
+          placeholder="이메일"
+          rules={{
+            required: AUTH_ERROR_MSG.EMAIL_REQUIRED,
+            pattern: {
+              value: AUTH_PATTERN.EMAIL,
+              message: AUTH_ERROR_MSG.EMAIL_PATTERN,
+            },
+          }}
+        />
+        <InputField
+          type="password"
+          label="비밀번호"
+          id="password"
+          name="password"
+          placeholder="비밀번호"
+          rules={{
+            required: AUTH_ERROR_MSG.PASSWORD_REQUIRED,
+          }}
+        />
+        <button type="submit">로그인</button>
+      </form>
+    </FormProvider>
   );
 };
 
