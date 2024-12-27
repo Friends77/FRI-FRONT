57ae476da980ae00bde89a37b603c3f4d2f395d6
itsmesunky <itisp_06@naver.com>
2024-12-27 23:46:14 +0900

    remove: index.css 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RecoilRoot } from "recoil";
-import "./index.css";
 import AppRouter from "./routes/AppRouter.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/apis/@core/queryClient.ts";
