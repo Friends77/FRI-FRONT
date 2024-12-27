eca5e4b4c974f1cd4757d1ab29636ada056a5647
itsmesunky <itisp_06@naver.com>
2024-12-27 17:43:17 +0900

    refactor: indent

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,17 +1,17 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
-import PretendardBold from "@/assets/fonts/Pretendard-Bold.woff";
-import PretendardRegular from "@/assets/fonts/Pretendard-Regular.woff";
+import PretendardBold from '@/assets/fonts/Pretendard-Bold.woff';
+import PretendardRegular from '@/assets/fonts/Pretendard-Regular.woff';
 
 const GlobalStyle = createGlobalStyle`
     @font-face {
-    font-family: Pretendard-Bold;
-    src: url(${PretendardBold}) format('woff');
+        font-family: Pretendard-Bold;
+        src: url(${PretendardBold}) format('woff');
     }
 
     @font-face {
-    font-family: Pretendard-Regular;
-    src: url(${PretendardRegular}) format('woff');
+        font-family: Pretendard-Regular;
+        src: url(${PretendardRegular}) format('woff');
     }
 `;
 
