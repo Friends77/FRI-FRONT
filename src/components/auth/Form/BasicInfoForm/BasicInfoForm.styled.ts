e28e739b0611c3c029f5a6c574c2bf8bf27c45a5
itsmesunky <itisp_06@naver.com>
2025-01-05 14:44:31 +0900

    feat: 프로필 작성(1) 폼

---
+++ src/components/auth/Form/BasicInfoForm/BasicInfoForm.styled.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export const BasicInfoFormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 120px;
+`;
+
+export const BasicInfoFormHeader = styled.section`
+  ${({ theme }) => theme.typo.D1_B};
+  color: ${({ theme }) => theme.colors.Gray_1000};
+`;
+
+export const BasicInfoFormContentSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  gap: 40px;
+  margin-top: 66px;
+`;
+
+export const BasicInfoFormImagePickerSection = styled.section`
+  display: flex;
+  justify-content: center;
+`;
+
+export const BasicInfoFormBirthSetion = styled.section`
+  position: relative;
+`;
+
+export const BasicInfoFormLabel = styled.label`
+  ${({ theme }) => theme.typo.T2_B};
+  color: ${({ theme }) => theme.colors.Gray_1000};
+
+  &::after {
+    content: '*';
+    position: absolute;
+    color: ${({ theme }) => theme.colors.Alter_error};
+  }
+`;
+
+export const BasicInfoFormGenderSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const BasicInfoFormRadio = styled.article`
+  display: flex;
+  gap: 88px;
+`;
