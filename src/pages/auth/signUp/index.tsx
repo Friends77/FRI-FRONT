7a9922592fa810895656442040bd7f4d93ac2361
itsmesunky <itisp_06@naver.com>
2025-01-05 14:46:22 +0900

    feat: 회원가입 폼 기본값 정의

--- src/pages/auth/signUp/index.tsx
+++ src/pages/auth/signUp/index.tsx
@@ -1,28 +1,36 @@
-import AuthForm from "@/components/auth/Form/AuthForm";
-import ProfileForm from "@/components/auth/Form/ProfileForm";
-import signUpStepAtom from "@/recoil/auth/signUp/atom";
-import { SignUpDataType } from "@/types/auth";
-import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
-import { useRecoilState } from "recoil";
+import AdditionalInfoForm from '@/components/auth/Form/AdditionalInfoForm';
+import AuthForm from '@/components/auth/Form/AuthForm';
+import BasicInfoForm from '@/components/auth/Form/BasicInfoForm';
+import signUpStepAtom from '@/recoil/auth/signUp/atom';
+import { SignUpDataType } from '@/types/auth';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { useRecoilState } from 'recoil';
 
 const SignUpPage = () => {
   // 회원가입 단계를 전역 상태로 관리
   const [signUpStep] = useRecoilState(signUpStepAtom);
 
   const methods = useForm<SignUpDataType>({
-    mode: "onChange",
+    mode: 'onChange',
     defaultValues: {
-      email: "",
-      certno: "",
-      password: "",
-      "confirm-password": "",
+      email: '',
+      certno: '',
+      password: '',
+      'confirm-password': '',
+      nickname: '',
+      gender: '',
+      EI: 'E',
+      NS: 'N',
+      FT: 'F',
+      JP: 'J',
     },
   });
 
   const { handleSubmit } = methods;
 
   const onSubmit: SubmitHandler<SignUpDataType> = (data) => {
-    console.log(data);
+    const mbti = `${data.EI}${data.NS}${data.FT}${data.JP}`;
+    console.log(mbti);
   };
 
   const renderPage = () => {
@@ -30,7 +38,9 @@ const SignUpPage = () => {
       case 1:
         return <AuthForm />;
       case 2:
-        return <ProfileForm />;
+        return <BasicInfoForm />;
+      case 3:
+        return <AdditionalInfoForm />;
       default:
         return; // TO-DO: 에러 페이지 추가
     }
