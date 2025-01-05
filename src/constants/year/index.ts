163c90d23b809bd4f616da7c224af731eef15282
itsmesunky <itisp_06@naver.com>
2025-01-05 14:45:52 +0900

    feat: 상수 정의

---
+++ src/constants/year/index.ts
@@ -0,0 +1,10 @@
+const startYear = 1920;
+const currentYear = new Date().getFullYear();
+
+export const BIRTH_YEAR = Array.from(
+  { length: currentYear - startYear + 1 },
+  (_, index) => {
+    const year: number = startYear + index;
+    return { value: year, label: `${year}` };
+  },
+);
