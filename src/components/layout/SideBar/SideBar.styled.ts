ea62952a72154e8b2a1525ef4bb9b2f4971b58a1
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-01-08 11:34:01 +0900

    feat: 비로그인 상태 구현

--- src/components/layout/SideBar/SideBar.styled.ts
+++ src/components/layout/SideBar/SideBar.styled.ts
@@ -6,9 +6,32 @@ export const Wrapper = styled.aside<{ $isOpen: boolean }>`
   left: 72px;
   width: 400px;
   height: 100%;
-  background-color: ${({ theme }) => theme.colors.White};
+  background-color: ${({ theme }) => theme.colors.Gray_100};
   transform: ${({ $isOpen }) =>
     $isOpen ? 'translateX(0)' : 'translateX(-100%)'};
   transition: transform 0.3s ease;
+  border-top-right-radius: 24px;
+  border-bottom-right-radius: 24px;
   z-index: 888;
+`;
+
+export const Header = styled.section`
+  padding: 18px 24px 14px;
+`;
+
+export const ProfileContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const ProfileImg = styled.img`
+  width: 52px;
+  height: 52px;
+  border-radius: 999px;
+`;
+
+export const Nickname = styled.p`
+  ${({ theme }) => theme.typo.T1_B}
+  color: ${({ theme }) => theme.colors.Gray_1000}
 `;
--- src/components/layout/SideBar/index.tsx
+++ src/components/layout/SideBar/index.tsx
@@ -1,11 +1,41 @@
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import * as Styled from './SideBar.styled';
 import isSideBarOpenAtom from '@/recoil/layout/isSideBarOpen';
+import defaultProfileImg from '@/assets/images/defaultProfile.png';
+import { useProfile } from '@/hooks/user/useProfile';
+import profileAtom from '@/recoil/user/profile';
+import { useEffect } from 'react';
 
 const SideBar = () => {
+  const { data } = useProfile();
   const isSideBarOpen = useRecoilValue(isSideBarOpenAtom);
+  const [profile, setProfile] = useRecoilState(profileAtom);
 
-  return <Styled.Wrapper $isOpen={isSideBarOpen}>SideBar</Styled.Wrapper>;
+  useEffect(() => {
+    if (data) {
+      setProfile(data);
+    }
+  }, [data, setProfile]);
+  return (
+    <Styled.Wrapper $isOpen={isSideBarOpen}>
+      <Styled.Header>
+        {profile ? (
+          <Styled.ProfileContent>
+            <Styled.ProfileImg src={profile?.imageUrl} alt="profile image" />
+            <Styled.Nickname>{profile?.nickname}</Styled.Nickname>
+          </Styled.ProfileContent>
+        ) : (
+          <Styled.ProfileContent>
+            <Styled.ProfileImg
+              src={defaultProfileImg}
+              alt="기본 프로필 이미지"
+            />
+            <Styled.Nickname>로그인이 필요해요.</Styled.Nickname>
+          </Styled.ProfileContent>
+        )}
+      </Styled.Header>
+    </Styled.Wrapper>
+  );
 };
 
 export default SideBar;
