1e21e61f70e24ab5a112b07fd28e3b3b5fcafcc3
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2025-01-16 22:40:23 +0900

    fix: 토큰 재발급(refresh) 실패 시 로그인 페이지로 이동
    - 비로그인 상태로 토큰 재발급 요청을 하면 발생하는 무한 루프를 해결했습니다.

--- src/components/auth/TokenRefresher/index.tsx
+++ src/components/auth/TokenRefresher/index.tsx
@@ -1,7 +1,10 @@
+import { AUTH_PATH } from '@/constants/routes';
 import { useRefresh } from '@/hooks/auth/useRefresh';
 import accessTokenAtom from '@/recoil/auth/accessToken';
 import isLoggedInAtom from '@/recoil/auth/isLoggedIn';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 import { useRecoilValue } from 'recoil';
 
 interface ITokenRefresherProps {
@@ -15,8 +18,11 @@ interface ITokenRefresherProps {
  *  @주요기능
  *      1) 서비스 접근 시 토큰 재발급
  *      2) 토큰 재발급(refresh) 요청 동안 로딩 처리
+ *      3) 토큰 재발급 실패 시 로그인 페이지로 이동
  */
 const TokenRefresher = ({ children }: ITokenRefresherProps) => {
+  const navigate = useNavigate();
+
   const isLoggedIn = useRecoilValue(isLoggedInAtom);
   const accessToken = useRecoilValue(accessTokenAtom);
   const [isLoading, setIsLoading] = useState(isLoggedIn && !accessToken);
@@ -27,9 +33,13 @@ const TokenRefresher = ({ children }: ITokenRefresherProps) => {
 
     const refresh = async () => {
       try {
-        // refresh api 요청
         await mutateAsync();
       } catch (err) {
+        if (axios.isAxiosError(err)) {
+          if (err.response?.status === 401) {
+            navigate(AUTH_PATH.LOGIN);
+          }
+        }
         console.log('Refresh Result:', err);
       } finally {
         setIsLoading(false);
