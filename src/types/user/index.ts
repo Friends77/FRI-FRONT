8743f8fc58c094b181164ab0a23554acddcb5cc3
itsmesunky <itisp_06@naver.com>
2025-01-19 18:18:46 +0900

    type: Options 타입 추가

--- src/types/user/index.ts
+++ src/types/user/index.ts
@@ -1,3 +1,5 @@
+import { Options } from '../@common';
+
 export interface Location {
   latitude: number;
   longitude: number;
@@ -35,7 +37,30 @@ export interface IProfileResponse {
     | 'ENTJ'
     | 'ESTJ';
   interestTag: InterestTag[];
-  imageUrl: 'string';
+  imageUrl: string;
+}
+
+// 실제 서버 전송 데이터 타입
+export interface UpdateProfileFormDataType {
+  imageUrl: string;
+  selfDescription: string;
+  birth: number;
+  gender: 'MAN' | 'WOMAN' | 'ETC';
+  mbti?: string;
+  interestTag?: (string | number)[];
+  location?: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
+// 폼 입력 데이터 타입
+export interface UpdateProfileDataType extends UpdateProfileFormDataType {
+  EI: string;
+  NS: string;
+  FT: string;
+  JP: string;
+  tags: Options[];
 }
 
 export type Profile = IProfileResponse;
