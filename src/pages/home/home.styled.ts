9cdbb56d0cfda4468af2bfe864e2773df022d996
itsmesunky <itisp_06@naver.com>
2025-01-23 16:57:24 +0900

    feat: 홈페이지: 추천 채팅방 리스트 영역

---
+++ src/pages/home/home.styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ChatRoomByTagSection = styled.section`
+  width: 865px;
+  height: 458px;
+`;
+
+export const ChatRoomByTagTitleSection = styled.section`
+  margin-bottom: 8px;
+`;
+
+export const ChatRoomByTagTitle = styled.h1`
+  ${({ theme }) => theme.typo.D2_B};
+  color: ${({ theme }) => theme.colors.Gray_1000};
+`;
+
+export const Highlight = styled.span`
+  ${({ theme }) => theme.typo.D2_B};
+  color: ${({ theme }) => theme.colors.Blue_500};
+`;
