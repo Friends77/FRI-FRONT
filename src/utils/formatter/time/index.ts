de595f424da64fa4cb24507d92a8cf211ec75868
itsmesunky <itisp_06@naver.com>
2025-01-31 17:47:52 +0900

    refactor: time format 함수 컨벤션 적용

---
+++ src/utils/formatter/time/index.ts
@@ -0,0 +1,8 @@
+export const formattedTime = (value: number) => {
+  const formattedSeconds = Math.floor(value / 1000);
+
+  const minutes = String(Math.floor(formattedSeconds / 60)).padStart(2, '0');
+  const seconds = String(Math.floor(formattedSeconds % 60)).padStart(2, '0');
+
+  return `${minutes}:${seconds}`;
+};
--- src/utils/formatter/time/index.tsx
+++
@@ -1,8 +0,0 @@
-export function formattedTime(value: number) {
-  const formattedSeconds = Math.floor(value / 1000);
-
-  const minutes = String(Math.floor(formattedSeconds / 60)).padStart(2, "0");
-  const seconds = String(Math.floor(formattedSeconds % 60)).padStart(2, "0");
-
-  return `${minutes}:${seconds}`;
-}
