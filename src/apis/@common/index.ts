b591ac5da6025ee075d690ed9ea80508c99d984a
itsmesunky <itisp_06@naver.com>
2025-02-02 19:48:57 +0900

    feat: 이미지 업로드 api 연동

--- src/apis/@common/index.ts
+++ src/apis/@common/index.ts
@@ -1,7 +1,7 @@
 import AuthAxios from '@/apis/@core/authInstance';
 
 export const imageUpload = async (formData: FormData) => {
-  const response = await AuthAxios.post<string>('/api/user/Image', formData);
+  const response = await AuthAxios.post<string>('/api/user/image', formData);
 
   return response.data;
 };
