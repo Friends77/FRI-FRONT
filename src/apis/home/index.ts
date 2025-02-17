296543940f70b2d683714706264f39ab83736f04
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-02-17 17:16:04 +0900

    feat: 사용자 선택 태그 기반 유저 추천 API 연동

--- src/apis/home/index.ts
+++ src/apis/home/index.ts
@@ -1,12 +1,28 @@
 import Axios from '@/apis/@core/instance';
-import { IChatRoomByTagResponse } from '@/types/home';
+import { IChatRoomByTagResponse, IFriendsByTagResponse } from '@/types/home';
 
+// 사용자 선택 태그 기반 추천 채팅방
 export const getChatRoomsByTag = async (id: number) => {
   const response = await Axios.get<IChatRoomByTagResponse>(
     '/api/global/recommendation/chat/category',
     {
       params: {
         categoryIds: id,
+      },
+    },
+  );
+
+  return response.data;
+};
+
+// 사용자 선택 태그 기반 추천 유저
+export const getFriendsByTag = async (id: number) => {
+  const response = await Axios.get<IFriendsByTagResponse>(
+    '/api/global/recommendation/user/category',
+    {
+      params: {
+        categoryIds: id,
+        size: 8,
       },
     },
   );
