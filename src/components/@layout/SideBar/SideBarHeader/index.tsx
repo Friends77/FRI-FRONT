4fc15369ee6b7515cefc4e3ca7570c0f4f759f0b
jiwon <iamkjw77@gmail.com>
2025-02-22 23:12:56 +0900

    feat: 마이메뉴 구현

--- src/components/@layout/SideBar/SideBarHeader/index.tsx
+++ src/components/@layout/SideBar/SideBarHeader/index.tsx
@@ -3,26 +3,15 @@ import { Suspense } from 'react';
 import Notification from '@/components/@common/SVG/Icon/Notification';
 import SideBarProfile from '../SideBarProfile';
 import SideBarProfileSkeleton from '../SideBarProfileSkeleton';
-import { useRecoilValue } from 'recoil';
-import isLoggedInAtom from '@/recoil/auth/isLoggedIn';
 import Plus from '@/components/@common/SVG/Icon/Plus';
 import { useNavigate } from 'react-router';
 import { CHAT_PATH } from '@/constants/routes';
-import { useLogout } from '@/hooks/auth/useLogout';
 
 const SideBarHeader = () => {
-  const { mutate: logout } = useLogout();
-
   const navigate = useNavigate();
-
-  const isLoggedIn = useRecoilValue(isLoggedInAtom);
 
   const handleCreateChatRoom = () => {
     navigate(CHAT_PATH.CHAT_ROOM_CREATE);
-  };
-
-  const handleLogout = () => {
-    logout();
   };
 
   return (
@@ -30,20 +19,15 @@ const SideBarHeader = () => {
       <Suspense fallback={<SideBarProfileSkeleton />}>
         <SideBarProfile />
       </Suspense>
-      {isLoggedIn && (
-        <Styled.SideBarButtonContainer>
-          <button type="button" onClick={handleLogout}>
-            로그아웃
-          </button>
-          <Styled.SideBarButton type="button" onClick={handleCreateChatRoom}>
-            <Plus title="채팅방 생성" width="32" height="32" />
-          </Styled.SideBarButton>
-          <Styled.SideBarButton type="button">
-            {/* 알림이 있는 경우 && <Styled.NotificationBadge /> */}
-            <Notification title="알림" width="32" height="32" />
-          </Styled.SideBarButton>
-        </Styled.SideBarButtonContainer>
-      )}
+      <Styled.SideBarButtonContainer>
+        <Styled.SideBarButton type="button" onClick={handleCreateChatRoom}>
+          <Plus title="채팅방 생성" width="32" height="32" />
+        </Styled.SideBarButton>
+        <Styled.SideBarButton type="button">
+          {/* 알림이 있는 경우 && <Styled.NotificationBadge /> */}
+          <Notification title="알림" width="32" height="32" />
+        </Styled.SideBarButton>
+      </Styled.SideBarButtonContainer>
     </Styled.Wrapper>
   );
 };
--- src/components/@layout/SideBar/SideBarProfile/index.tsx
+++ src/components/@layout/SideBar/SideBarProfile/index.tsx
@@ -2,13 +2,37 @@ import { useProfile } from '@/hooks/user/useProfile';
 import * as Styled from './SideBarProfile.styled';
 import { useSetRecoilState } from 'recoil';
 import profileAtom from '@/recoil/user/profile';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ProfileImage from '@/components/@common/ProfileImage';
+import Person from '@/components/@common/SVG/Icon/Person';
+import Logout from '@/components/@common/SVG/Icon/Logout';
+import { useLogout } from '@/hooks/auth/useLogout';
+import { USER_PATH } from '@/constants/routes';
+import { useNavigate } from 'react-router';
 
 const SideBarProfile = () => {
+  const navigate = useNavigate();
+
   const { data } = useProfile();
 
   const setProfile = useSetRecoilState(profileAtom);
+
+  const [isOpenMyMenu, setIsOpenMyMenu] = useState(false);
+
+  const { mutate: logout } = useLogout();
+
+  const handleToggleMyMenu = () => {
+    setIsOpenMyMenu((prevState) => !prevState);
+  };
+
+  const handleGoMyPage = () => {
+    setIsOpenMyMenu(false);
+    navigate(USER_PATH.PROFILE);
+  };
+
+  const handleLogout = () => {
+    logout();
+  };
 
   useEffect(() => {
     if (data) {
@@ -17,10 +41,28 @@ const SideBarProfile = () => {
   }, [data, setProfile]);
 
   return (
-    <Styled.ProfileContent>
-      <ProfileImage src={data.imageUrl} alt="profile image" size={52} />
-      <Styled.Nickname>{data.nickname}</Styled.Nickname>
-    </Styled.ProfileContent>
+    <>
+      {isOpenMyMenu && (
+        <Styled.MyMenu>
+          <Styled.MyPage>
+            <Styled.MyPageButton type="button" onClick={handleGoMyPage}>
+              <Person title="마이페이지" width="24" height="24" />
+              마이페이지
+            </Styled.MyPageButton>
+          </Styled.MyPage>
+          <Styled.Logout>
+            <Styled.LogoutButton type="button" onClick={handleLogout}>
+              <Logout title="로그아웃" width="24" height="24" />
+              로그아웃
+            </Styled.LogoutButton>
+          </Styled.Logout>
+        </Styled.MyMenu>
+      )}
+      <Styled.ProfileContent type="button" onClick={handleToggleMyMenu}>
+        <ProfileImage src={data.imageUrl} alt="profile image" size={52} />
+        <Styled.Nickname>{data.nickname}</Styled.Nickname>
+      </Styled.ProfileContent>
+    </>
   );
 };
 
